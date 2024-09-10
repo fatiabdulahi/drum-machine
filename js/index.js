d8a21f86c45df8314daabda41c3eaea20ea5f0b0
fatiabdulahi <168986987+fatiabdulahi@users.noreply.github.com>
2024-09-10 05:48:42 -0700

    eslint error

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 const createClass = function () {
   function defineProperties(target, props) {
-    for (let i = 0; i < props.length; i++) {
-      let descriptor = props[i];
+    for (let i = 0; i < props.length; i+=1) {
+      const descriptor = props[i];
       descriptor.enumerable = descriptor.enumerable || false;
       descriptor.configurable = true;
-      if ("value" in descriptor) descriptor.writable = true;
+      if ('value' in descriptor) descriptor.writable = true;
       Object.defineProperty(target, descriptor.key, descriptor);
     }
   }
@@ -14,21 +14,26 @@ const createClass = function () {
     return Constructor;
   };
 }();
-function _classCallCheck(instance, Constructor) {
+function classCallCheck(instance, Constructor) {
   if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
+    throw new TypeError('Cannot call a class as a function');
   }
 }
-function _possibleConstructorReturn(self, call) {
+function possibleConstructorReturn(self, call) {
   if (!self) {
     throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
   }
   return call && (typeof call === "object" || typeof call === "function") ? call : self;
 }
-function _inherits(subClass, superClass) {
+function inherits(subClass, superClass) {
   if (typeof superClass !== "function" && superClass !== null) {
-    throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
-  }subClass.prototype = Object.create(superClass && superClass.prototype, {constructor: { value: subClass, enumerable: false, writable: true, configurable: true } });
+    throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass);
+  }
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: { value: subClass, enumerable: false, writable: true, configurable: true 
+
+    }
+  });
   if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
 }
 var drumAudio = [//9 drum elements with src attribute for audio element
@@ -43,7 +48,7 @@ var drumAudio = [//9 drum elements with src attribute for audio element
 { id: 'Heater 3', padLetter: 'C', keyCode: 67, src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3', type: 'audio/mp3' }];var
 
 
-DrumPad = function (_React$Component) {_inherits(DrumPad, _React$Component);function DrumPad() {var _ref;var _temp, _this, _ret;_classCallCheck(this, DrumPad);for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {args[_key] = arguments[_key];}return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = DrumPad.__proto__ || Object.getPrototypeOf(DrumPad)).call.apply(_ref, [this].concat(args))), _this), _this.    handleKeyDown = function (event) {
+DrumPad = function (_React$Component) {inherits(DrumPad, _React$Component);function DrumPad() {var _ref;var _temp, _this, _ret;classCallCheck(this, DrumPad);for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {args[_key] = arguments[_key];}return _ret = (_temp = (_this = possibleConstructorReturn(this, (_ref = DrumPad.__proto__ || Object.getPrototypeOf(DrumPad)).call.apply(_ref, [this].concat(args))), _this), _this.    handleKeyDown = function (event) {
       if (event.keyCode === _this.props.padLetter.charCodeAt()) {
         _this.audio.play();
         _this.audio.currentTime = 0; //set time audio should play at (0 seconds)
@@ -55,7 +60,7 @@ DrumPad = function (_React$Component) {_inherits(DrumPad, _React$Component);func
       _this.audio.play();
       _this.audio.currentTime = 0; //set time audio should play at (0 seconds)
       _this.props.handleDisplay(_this.props.id); //changes value of parameter used in display function
-    }, _temp), _possibleConstructorReturn(_this, _ret);}createClass(DrumPad, [{ key: 'componentDidMount', //method invoked immediately after component mounted using event listener
+    }, _temp), possibleConstructorReturn(_this, _ret);}createClass(DrumPad, [{ key: 'componentDidMount', //method invoked immediately after component mounted using event listener
     value: function componentDidMount() {document.addEventListener('keydown', this.handleKeyDown); //window.focus()//set focus to the current window
     } //method for when component to be removed from DOM
   }, { key: 'componentWillUnmount', value: function componentWillUnmount() {document.removeEventListener('keydown', this.handleKeyDown);} //method to handle a key down event from keyboard - play HTML audio and change display
@@ -69,8 +74,11 @@ DrumPad = function (_React$Component) {_inherits(DrumPad, _React$Component);func
     } }]);return DrumPad;}(React.Component);
 //end of class
 var
-App = function (_React$Component2) {_inherits(App, _React$Component2);
-  function App(props) {_classCallCheck(this, App);var _this3 = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this,
+App = function (_React$Component2) {
+  inherits(App, _React$Component2);
+  function App(props) {
+    classCallCheck(this, App);
+    var _this3 = possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this,
     props));_this3.
 
 
