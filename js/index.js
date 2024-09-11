7053c4cdec63233e796ee92c53790db10bd92e45
fatiabdulahi <168986987+fatiabdulahi@users.noreply.github.com>
2024-09-11 05:49:46 -0700

    eslint error

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const createClass = function () {
+const createClass = (function () {
   function defineProperties(target, props) {
     for (let i = 0; i < props.length; i += 1) {
       const descriptor = props[i];
@@ -13,26 +13,27 @@ const createClass = function () {
     if (staticProps) defineProperties(Constructor, staticProps);
     return Constructor;
   };
-}();
+})();
+
 function classCallCheck(instance, Constructor) {
   if (!(instance instanceof Constructor)) {
     throw new TypeError('Cannot call a class as a function');
   }
 }
+
 function possibleConstructorReturn(self, call) {
   if (!self) {
     throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
   }
   return call && (typeof call === 'object' || typeof call === 'function') ? call : self;
 }
+
 function inherits(subClass, superClass) {
   if (typeof superClass !== "function" && superClass !== null) {
     throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass);
   }
   subClass.prototype = Object.create(superClass && superClass.prototype, {
-    constructor: { value: subClass, enumerable: false, writable: true, configurable: true 
-
-    }
+    constructor: { value: subClass, enumerable: false, writable: true, configurable: true }
   });
   if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
 }
